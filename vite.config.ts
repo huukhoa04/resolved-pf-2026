080b69f7ac00b3e3d9171d52b0c5cafc03429274
huukhoa04 <huukhoa04@gmail.com>
2026-01-29 22:42:28 +0700

    fix: adjust base cwd

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/resolved-pf-2026/",
+  base: "./",
   plugins: [react(), tailwindcss()],
 });
