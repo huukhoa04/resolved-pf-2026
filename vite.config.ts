b1bc2ecc10ff851a1c5f4b4eaeae31b360347425
huukhoa04 <huukhoa04@gmail.com>
2026-01-29 22:35:32 +0700

    build: add gh action

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/resolved-pf-2026/",
   plugins: [react(), tailwindcss()],
 });
